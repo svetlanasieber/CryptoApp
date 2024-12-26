89dfbd6abba3ed159f4b2ae6ece60179b1476acb
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-26 23:23:57 +0100

    Create CryptoTreadingApplicationTests.java

---
+++ src/test/java/com/sve/CryptoTreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
